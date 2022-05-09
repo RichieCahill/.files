2532407d32549c71829a87bc1933e8b56a61f742
Richie Cahill <richie@tmmworkshop.com>
2022-05-09 03:14:24 -0400

    reworked update.sh with a switch case and better os detection

--- update.sh
+++ update.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 while :; do
-		case $1 in
-				-R|--reboot) Reboot="True"
-				;;
-				-S|--shutdown) Shutdown="True"
-				;;
-				*) break
-		esac
-		shift
+	case $1 in
+		-R|--reboot) Reboot="True"
+		;;
+		-S|--shutdown) Shutdown="True"
+		;;
+		*) break
+	esac
+	shift
 done
 
-OS=$(uname -a)
+#Gets the os information
+OS=$(cat /etc/os-release)
 
-#Checks if the system is arch 
-if [[ $OS == *arch* ]]; then
-	#Checks if yay -s installed
-	if [[ $(which yay) == /usr/bin/yay ]]; then
-		#runs yay update
-		yay -Syu --noconfirm
-		yay -Yc
-	else
-		#Runs pacman update
-		pacman -Syu --noconfirm
-	fi
-#Checks if the system is Ubuntu 
-elif [[ $OS == *Ubuntu* ]]; then
-	#Runs apt update upgrad and auto remove
+#Runs the corect coman based on the OS ID
+case $OS in
+	*ID=arch*)
+		if [[ $(which yay) == /usr/bin/yay ]]; then
+			#runs yay update
+			yay -Syu --noconfirm
+			yay -Yc
+		else
+			#Runs pacman update if yay isnt present
+			pacman -Syu --noconfirm
+		fi
+	;;
+	*ID=ubuntu*|*ID=debian*|*ID=pop*)
+		#Runs apt update upgrade and autoremove
 	sudo apt update && sudo apt upgrade -y && sudo apt autoremove
-#Checks if the system is Debian
-elif [[ $OS == *Debian* ]]; then
-	#Runs apt update upgrad
-	sudo apt update && sudo apt upgrade -y
-fi
+	;;
+	*)
+esac
+
 #Reboot is flag is presed
 if [[ $Reboot == True ]]; then
 	sudo reboot
